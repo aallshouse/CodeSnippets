c736fded2c75119a892ba57264a22056bba30834
aallshouse <allshouseandrew@gmail.com>
2010-10-26 05:52:45 -0400

    Add new files

---
+++ TestProject/TestProject/DbResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestProject
+{
+    public class DbResult
+    {
+        public int TableID { get; set; }
+        public string SomeValue1 { get; set; }
+        public string SomeValue2 { get; set; }
+    }
+}
